4b2dd9a7dd64569180b6057c85c654b13526990d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 16:08:46 +0530

    JDBC steps

--- src/main/java/com/registration/servlet/RegistrationForm.java
+++ src/main/java/com/registration/servlet/RegistrationForm.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * @author Naveen Wodeyar
@@ -21,11 +23,27 @@ public class RegistrationForm extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		doGet(request, response);
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-		String gender = request.getParameter("gender");
-		String city = request.getParameter("city");
-		String password = request.getParameter("password");
+		
+		// retrieve the values from request objet,
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String gender = request.getParameter("gender");
+			String city = request.getParameter("city");
+			String password = request.getParameter("password");
+		
+		// Establish the connection,
+			try{
+				// 1. Load the driver,
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				// 2. Establish the connection,
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/reg2023");
+				
+				// 3. 
+			}
+			catch (Exception e) {
+				System.out.println(e.getCause());
+			}
 		
 	}
 
