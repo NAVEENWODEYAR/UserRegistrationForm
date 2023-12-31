b51577c66b1fbf615a9235bfeeef7a6c55927433
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 16:21:02 +0530

    ps.executeUpdate

--- src/main/java/com/registration/servlet/RegistrationForm.java
+++ src/main/java/com/registration/servlet/RegistrationForm.java
@@ -1,13 +1,16 @@
 package com.registration.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  * @author Naveen Wodeyar
@@ -39,7 +42,32 @@ public class RegistrationForm extends HttpServlet {
 				// 2. Establish the connection,
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/reg2023");
 				
-				// 3. 
+				// 3. Prepare the statement,
+				PreparedStatement ps = con.prepareStatement("INSERT INTO register VALUES(?,?,?,?,?)");
+									ps.setString(0, name);
+									ps.setString(1, email);
+									ps.setString(2, gender);
+									ps.setString(3, city);
+									ps.setString(4, password);
+						int executeUpdate = ps.executeUpdate();
+						
+						if(executeUpdate > 0){
+							response.setContentType("text/html");
+							PrintWriter pw = response.getWriter();
+							pw.print("<h2> User registration successfull</h2>");
+							
+							RequestDispatcher rd = request.getRequestDispatcher("Register.jsp");
+												rd.include(request, response);
+						}
+						else {
+							response.setContentType("text/html");
+							PrintWriter pw = response.getWriter();
+							pw.print("<h2 style:'red'> User registration failed</h2>");
+							
+							RequestDispatcher rd = request.getRequestDispatcher("Register.jsp");
+												rd.include(request, response);
+						}
+							
 			}
 			catch (Exception e) {
 				System.out.println(e.getCause());
